234e253b91bb9af943d2735014ca279d862f6bd4
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-16 20:33:50 -0400

    Add pseudocode stub

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,13 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
+  get: function (req, res) {
+    // check request method?
+
+    // query the database and get all the messages
+
+    // send a response with the messages
+
+  }, // a function which handles a get request for all messages
   post: function (req, res) {} // a function which handles posting a message to the database
 };
